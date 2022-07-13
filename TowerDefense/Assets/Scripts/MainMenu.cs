563ece8b12b15c9bc788c05470012d95031b0816
eharris733 <elliotmharris@gmail.com>
2022-07-12 23:13:43 -0400

    Added orcs, main menu
    read the title

---
+++ TowerDefense/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Menu() {
+        SceneManager.LoadScene(0);
+    }
+    public void Level1(){
+        SceneManager.LoadScene(1);
+    }
+
+
+}
